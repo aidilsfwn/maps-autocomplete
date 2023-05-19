2c0094c07c0aaa8a078c6228b2c8dd3c3bf1d8be
Aidil Safwan <aidil.safwan@aleph-labs.com>
2023-05-19 22:19:31 +0800

    Create backend server to retrieve data from places api

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+const dotenv = require('dotenv')
+const path = require('path')
+
+dotenv.config({ path: path.join(__dirname, './.env') })
+
+const app = express()
+
+// parse json request body
+app.use(express.json())
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }))
+
+// enable cors
+app.use(cors())
+app.options('*', cors())
+
+app.get('/api/getAutoComplete', async (req, res) => {
+  try {
+    let input = req.query.input
+    const { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/queryautocomplete/json?input=${input}&key=${process.env.MAPS_API_KEY}`)
+    res.send(data)
+  } catch (e) {
+    console.log({ e })
+  }
+})
+
+app.get('/api/getPlaceDetails', async (req, res) => {
+  try {
+    let placeID = req.query.placeID
+    const { data } = await axios.get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeID}&key=${process.env.MAPS_API_KEY}`)
+    res.send(data)
+  } catch (e) {
+    console.log({ e })
+  }
+})
+
+app.listen('3001', () => {
+  console.log('Server running on port 3001')
+})
